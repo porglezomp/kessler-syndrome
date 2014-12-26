4b2b1c0a7c05bc38b2f79f9a4af1599265c70aaa
Caleb Jones <bionicle.dragon@gmail.com>
2014-12-26 00:24:49 -0500

    Refactor game.c to use `struct rocket`
    Move to C99 (gnu99 to avoid conflicts with C89 rpi headers)
    * Use for struct initialization
    Add fuel requirements
    * Engines need fuel to run
    * Add fuel (main and RCS) information to rocket struct
    * Add fuel gage rendering

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 LIBS = $(shell pkg-config PiGL --libs) -lm
-CFLAGS = $(shell pkg-config PiGL --cflags)
+CFLAGS = $(shell pkg-config PiGL --cflags) --std=gnu99 -fgnu89-inline
 SRC = game.c game_input.c
 OBJS = game.o game_input.o
 APP = Game.out
--- game.c
+++ game.c
@@ -10,6 +10,9 @@
 #include <linux/input.h>
 #include <math.h>
 
+void panic();
+void input();
+
 void panic() {
     fflush(stdout);
     fprintf(stderr, "%s.\n", strerror(errno));
@@ -25,18 +28,15 @@ void cleanup() {
     printf("\033[H\033[2J");
 }
 
-static float pos_x = 0;
-static float pos_y = 0;
-static float force = 0.0001;
-
 #define NUM_BODY   4
 #define START_BODY 0
 #define NUM_THRUST 3
 #define MAIN_THRUST_START  4
 #define LEFT_THRUST_START  7
 #define RIGHT_THRUST_START 10
+#define LINE_START 13
 
-static float data[NUM_BODY*2 + NUM_THRUST*2*3] = {
+static float data[NUM_BODY*2 + NUM_THRUST*2*3 + 4] = {
     // Body
     0,  0,
     1, -1,
@@ -53,22 +53,34 @@ static float data[NUM_BODY*2 + NUM_THRUST*2*3] = {
    // Left thrust
    .33, 2,
    1, 2.33,
-   .2, 2.5
+   .2, 2.5,
+   // Straight Line
+   0, 0,
+   1, 0
 };
 
-static float accel_x = 0;
-static float accel_y = 0;
-static float vel_x = 0;
-static float vel_y = 0;
-static float angle = 0;
-static float angle_vel = 0;
+struct rocket {
+    float accel_x, accel_y, vel_x, vel_y,
+          pos_x, pos_y, angle, angle_vel,
+          angle_force, thrust, max_rcs_fuel,
+          max_main_fuel, rcs_fuel, main_fuel,
+          rcs_fuel_rate, main_fuel_rate;
+    int damping;
+};
+
 static float input_x = 0;
 static float input_y = 0;
-const static float angle_force = 0.1;
-const static float bounce = 0.5;
-static int damping = 0;
 
-void input();
+static struct rocket ship = {
+    .angle_force = 0.1,
+    .thrust = 0.0001,
+    .max_rcs_fuel = 100,
+    .rcs_fuel = 100,
+    .rcs_fuel_rate = 0.02,
+    .max_main_fuel = 1000,
+    .main_fuel = 1000,
+    .main_fuel_rate = 0.3,
+};
 
 int main() {
     OGL_Init();
@@ -91,53 +103,83 @@ int main() {
         glClear(GL_COLOR_BUFFER_BIT);
 
         handle_input();
-
-        input_x = 0, input_y = 0, damping = 0;
+        input_x = 0, input_y = 0, ship.damping = 0;
         if (key_down(KEY_UP))    input_y += 1;
         if (key_down(KEY_DOWN))  input_y -= 1;
         if (key_down(KEY_LEFT))  input_x -= 1;
         if (key_down(KEY_RIGHT)) input_x += 1;
-        if (key_down(KEY_S))     damping = 1;
+        if (key_down(KEY_S))     ship.damping = 1;
 
         // Collide with the edges of the screen
-        if (pos_x > asp)  pos_x = asp;
-        if (pos_x < -asp) pos_x = -asp;
-        if (pos_y > 1)    pos_y = 1;
-        if (pos_y < -1)   pos_y = -1;
+        if (ship.pos_x > asp)  ship.pos_x = asp;
+        if (ship.pos_x < -asp) ship.pos_x = -asp;
+        if (ship.pos_y > 1)    ship.pos_y = 1;
+        if (ship.pos_y < -1)   ship.pos_y = -1;
 
-        glTranslatef(pos_x, pos_y, 0);
+        glTranslatef(ship.pos_x, ship.pos_y, 0);
         glScalef(0.05, 0.05, 0.05);
-        glRotatef(-angle, 0, 0, 1);
+        glRotatef(-ship.angle, 0, 0, 1);
 
         glDrawArrays(GL_LINE_LOOP, 0, NUM_BODY);
+        // Input also draws the thrust effects
         input();
+        glPopMatrix();
+
+        glPushMatrix();
+        glTranslatef(-asp + 0.1, 0.9, 0);
+        glPushMatrix();
+        glScalef(ship.main_fuel/ship.max_main_fuel, 1, 1);
+        glDrawArrays(GL_LINES, LINE_START, 2);
+        glPopMatrix();
+        glPushMatrix();
+        glScalef(ship.rcs_fuel/ship.max_rcs_fuel, 1, 1);
+        glTranslatef(0, -0.05, 0);
+        glDrawArrays(GL_LINES, LINE_START, 2);
+        glPopMatrix();
+        glPopMatrix();
 
         OGL_SwapBuffers();
-        glPopMatrix();
+        if (key_down(KEY_ESC)) running = 0;
     }
 
-    printf("%d %d\n", raspiGL_screen_width, raspiGL_screen_height);
     return EXIT_SUCCESS;
 }
 
 void input() {
-    if (damping) input_x += angle_vel < 0 ? .5 : -.5;
+    // Enable stabilization, fire opposite rotation
+    // (0.5 because half as powerful as normal thrusters)
+    if (ship.damping) input_x += ship.angle_vel < 0 ? .5 : -.5;
 
-    angle_vel += input_x * angle_force;
-    angle += angle_vel;
-    if (input_x > 0) {
-        glDrawArrays(GL_LINE_STRIP, LEFT_THRUST_START, NUM_THRUST);
-    } else if (input_x < 0) {
-        glDrawArrays(GL_LINE_STRIP, RIGHT_THRUST_START, NUM_THRUST);
-    }
-    if (input_y < 0) input_y = 0;
-    if (input_y > 0) {
-        glDrawArrays(GL_LINE_STRIP, MAIN_THRUST_START, NUM_THRUST);
-    }
-    accel_x = sin(angle*M_PI/180) * input_y * force;
-    accel_y = cos(angle*M_PI/180) * input_y * force;
-    vel_x += accel_x;
-    vel_y += accel_y;
-    pos_x += vel_x;
-    pos_y += vel_y;
+    // Maneuvering thrusters
+    if (ship.rcs_fuel > 0) {
+        ship.angle_vel += input_x * ship.angle_force;
+        // Draw the RCS thrusters if the rotation controls are enabled
+        if (input_x > 0) {
+            glDrawArrays(GL_LINE_STRIP, LEFT_THRUST_START, NUM_THRUST);
+            ship.rcs_fuel -= ship.rcs_fuel_rate * input_x;
+        } else if (input_x < 0) {
+            glDrawArrays(GL_LINE_STRIP, RIGHT_THRUST_START, NUM_THRUST);
+            ship.rcs_fuel -= ship.rcs_fuel_rate * -input_x;
+        }
+    } else if (ship.rcs_fuel < 0) ship.rcs_fuel = 0;
+
+
+    // Linear thruster
+    if (ship.main_fuel > 0) {
+        // Don't let the thruster be fired backwards
+        if (input_y < 0) input_y = 0;
+        if (input_y > 0) {
+            glDrawArrays(GL_LINE_STRIP, MAIN_THRUST_START, NUM_THRUST);
+            ship.main_fuel -= ship.main_fuel_rate * input_y;
+        }
+        ship.accel_x = sin(ship.angle*M_PI/180) * input_y * ship.thrust;
+        ship.accel_y = cos(ship.angle*M_PI/180) * input_y * ship.thrust;
+    } else if (ship.main_fuel < 0) ship.main_fuel = 0;
+
+    // Perform the physics
+    ship.vel_x += ship.accel_x;
+    ship.vel_y += ship.accel_y;
+    ship.pos_x += ship.vel_x;
+    ship.pos_y += ship.vel_y;
+    ship.angle += ship.angle_vel;
 }
